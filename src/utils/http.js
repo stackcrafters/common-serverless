928412bc7ed1d2c78b3fb0f740ec06f9cdff72b7
j3tto <james@stackcrafters.com>
2021-02-09 15:31:37 +0000

    Added Additional Headers Args to response method

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,9 +1,10 @@
-export const response = (body, statusCode = code.OK) => {
+export const response = (body, statusCode = code.OK, additionalHeaders = {}) => {
   return {
     statusCode: statusCode,
     body: JSON.stringify(body),
     headers: {
-      'Access-Control-Allow-Origin': '*'
+      'Access-Control-Allow-Origin': '*',
+      ...additionalHeaders
     }
   };
 };
